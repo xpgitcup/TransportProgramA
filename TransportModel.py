1de40ed913e963c8c8ee21c8952a8bfcf9a4a6e0
LiXiaoping <xpli126@126.com>
2018-10-22 00:04:52 +0800

    已经实现了西北角法，正在编辑最小元素法

--- TransportModel.py
+++ TransportModel.py
@@ -6,13 +6,17 @@ class TransportModel:
     sales = []  # 销地销量
     saleNames = []
     prices = []  # 价格
-    numberOfSource = 0
-    numberOfSale = 0
+    numberOfSources = 0
+    numberOfSales = 0
+    transProject = []
+    totalProdtion = 0
+    totalSale = 0
+    leftProduction = 0
 
     def initModel(self, dataLines):
         # 维度
-        self.numberOfSource = len(dataLines) - 2  # 产地 = 行数 - 1
-        self.numberOfSale = len(dataLines[0].split()) - 2  # 销地 = 列数 - 2
+        self.numberOfSources = len(dataLines) - 2  # 产地 = 行数 - 1
+        self.numberOfSales = len(dataLines[0].split()) - 2  # 销地 = 列数 - 2
         # 开始识别
         for i in range(len(dataLines)):
             line = dataLines[i]
@@ -20,46 +24,133 @@ class TransportModel:
             if i == 0:  # 识别销地
                 for j in range(len(cols)):
                     if j > 0:
-                        if j < self.numberOfSale + 1:   # 销地名称
+                        if j < self.numberOfSales + 1:   # 销地名称
                             self.saleNames.append(cols[j])
                     else:
                         name = cols[j]  # 名字--问题的名称
             else:
-                if i < self.numberOfSource + 1:
+                if i < self.numberOfSources + 1:
                     # 开始识别价格
                     v = []
                     for j in range(len(cols)):
                         if j == 0:
                             self.sourceNames.append(cols[j])
                         else:
-                            if j < self.numberOfSale + 1:   # 价格
+                            if j < self.numberOfSales + 1:   # 价格
                                 v.append(float(cols[j]))
                             else:
                                 self.sources.append(float(cols[j]))
                     self.prices.append(v)
                 else:
                     for j in range(len(cols)):
-                        if (j>0 and j < self.numberOfSale + 1):
+                        if (j>0 and j < self.numberOfSales + 1):
                             self.sales.append(float(cols[j]))
 
-        print("共有%d产地：" % (self.numberOfSource), self.sourceNames)
+        print("共有%d产地：" % (self.numberOfSources), self.sourceNames)
         print("产量：", self.sources)
-        print("共有%d销地：" % (self.numberOfSale), self.saleNames)
+        print("共有%d销地：" % (self.numberOfSales), self.saleNames)
         print("销量：", self.sales)
         print("运价表：")
         print(self.prices)
+        self.totalProdtion = sum(self.sources)
+        self.totalSale = sum(self.sales)
+        self.leftProduction = self.totalProdtion
         return
 
+    def showTransProject(self):
+        for e in self.transProject:
+            print(e)
+
     def initTransportProject(self):
+
+        # 初始化一个空的调运方案
+        def initEmptyTransportProject(self):
+            for i in range(0, self.numberOfSources):
+                pe = []
+                for j in range(0, self.numberOfSales):
+                    p = {}
+                    p["value"] = 0.0
+                    p["isBase"] = False
+                    pe.append(p)
+                self.transProject.append(pe)
+            print("空调运方案：")
+            self.showTransProject()
+            return
 
         # 西北角法
         def northwestCornerIteration(self):
             print("西北角法初始调运方案：")
+            # 从西北角开始
+
+            rowIndex = 0
+            colIndex = 0
+            tempProduction = self.sources.copy()
+            tempSalesVolume = self.sales.copy()
+            while ((self.leftProduction > 0) and (rowIndex < self.numberOfSources) and (colIndex < self.numberOfSales)):
+                row = False
+                col = False
+                # 查找调运量 - -与横纵坐标相关
+                transValue = min(tempProduction[rowIndex], tempSalesVolume[colIndex])
+                # 填写调运方案
+                self.transProject[rowIndex][colIndex]["value"] = transValue
+                self.transProject[rowIndex][colIndex]["isBase"] = True
+                # 登记完成的行列
+                tempProduction[rowIndex] -= transValue
+                tempSalesVolume[colIndex] -= transValue
+
+                if (tempProduction[rowIndex] == 0):
+                    row = True
+                    rowIndex += 1
+                if (tempSalesVolume[colIndex] == 0):
+                    col = True
+                    colIndex += 1
+                # 如果同时行列都满足，添加一个零(同列)
+                if (row and col):
+                    if ((rowIndex < self.numberOfSources) and (colIndex < self.numberOfSales)):
+                        self.transProject[rowIndex][colIndex - 1]["value"] = 0
+                        self.transProject[rowIndex][colIndex - 1]["isBase"] = True
+                # 更新剩余产量
+                self.leftProduction -= transValue
             return
 
         #最小元素法
         def miniElementIteration(self):
             print("最小元素法初始调运方案：")
+            # 查找最小元素
+            tempProduction = self.sources.copy()
+            tempSalesVolume = self.sales.copy()
+            miniE = findMiniElement(price, finishedSource, finishedTarget)
+            stepIndex + +
+
+            // 查找调运量 - -与横纵坐标相关
+
+            def transValue =
+
+            Math.min(tempProduction[miniE.i], tempSalesVolume[miniE.j])
+
+            // 填写调运方案
+            transProject[miniE.i][miniE.j] = transValue
+            transProjectFlag[miniE.i][miniE.j] = true
+            calculateTotalCost()
+
+            // 登记完成的行列
+            if (transValue == tempProduction[miniE.i]) {
+            finishedSource.add(miniE.i)
+            }
+            tempProduction[miniE.i] -= transValue
+
+            if (transValue == tempSalesVolume[miniE.j]) {
+            finishedTarget.add(miniE.j)
+            }
+            tempSalesVolume[miniE.j] -= transValue
+
+            if (tempProduction[miniE.i] == tempSalesVolume[miniE.j]) {
+            // 同时划去一行 & 一列
+            println("同时满足：${stepIndex}  ${miniE}")
+            }
+
+            // 更新剩余产量
+            leftProduction -= transValue
 
             return
 
@@ -68,10 +159,12 @@ class TransportModel:
             '2': miniElementIteration
         }
 
+        initEmptyTransportProject(self)
         methodIndex = input("请选择初始化方法(1--西北角, 2--最小元素法)：")
         if (methodIndex==''):
             methodIndex = '2'
         initMethod[methodIndex](self)
+        self.showTransProject()
 
         return
 
